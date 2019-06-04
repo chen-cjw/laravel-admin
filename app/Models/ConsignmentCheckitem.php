65005ce90e7dd23016399acdaae3f03477f4675e
1350359783@qq.com <1350359783@qq.com>
2019-06-05 00:19:00 +0800

    新增样品单接口

--- app/Models/ConsignmentCheckitem.php
+++ app/Models/ConsignmentCheckitem.php
@@ -12,4 +12,10 @@ class ConsignmentCheckitem extends Model
         'name','unit','tech_req','test_value','test_method','orgs',
         'user_id','user_name','remark','update_time','create_time','sample_id'
     ];
+
+    public function addAll(Array $data)
+    {
+        $rs = DB::table($this->getTable())->insert($data);
+        return $rs;
+    }
 }
