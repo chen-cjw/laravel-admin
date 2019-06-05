e85e02eb6ab0d5cf2aa52fb9ac7901a18b4c3395
柚子 <106157421@qq.com>
2019-06-05 09:53:52 +0800

    去除多余的代码

--- app/Admin/Controllers/Controller.php
+++ app/Admin/Controllers/Controller.php
@@ -3,35 +3,11 @@
 namespace App\Admin\Controllers;
 
 use App\Http\Controllers\Controller as BaseController;
-use App\Model\Symbol;
 use Encore\Admin\Layout\Content;
 
 class Controller extends BaseController
 {
-    public function symbol($symbol,$amount)
-    {
-        switch ($symbol) {
-            case "BTC":
-                return $this->realBtc($symbol,$amount);
-            case "ETH":
-                return $this->realEth($symbol,$amount);
-            case "USD":
-                return $this->realUsd($symbol,$amount);
-            default:
-                return '未知币种';
-        }
 
-    }
-
-    public function time($grid)
-    {
-        $grid->created_at('创建时间')->display(function ($created_at) {
-            return date('Y-m-d H:i:s',$created_at);
-        })->sortable();
-        $grid->updated_at('编辑时间')->display(function ($created_at) {
-            return date('Y-m-d H:i:s',$created_at);
-        })->sortable();
-    }
     /**
      * Index interface.
      *
@@ -90,53 +66,6 @@ class Controller extends BaseController
             ->body($this->form());
     }
 
-    public function moreZero($amount)
-    {
-        return rtrim(rtrim($amount, '0'), '.');
-
-    }
-
-    public function realUsd($amount)
-    {
-        return $this->moreZero(bcdiv($amount, Symbol::USD_RATE, 4));
-    }
-
-    public function realBtc($amount)
-    {
-        return $this->moreZero(bcdiv($amount, Symbol::BTC_RATE, 9));
-
-    }
-
-    public function realEth($amount)
-    {
-        return $this->moreZero(bcdiv($amount, Symbol::ETH_RATE, 18));
-    }
-    public function realTP($amount)
-    {
-        return $this->moreZero(bcdiv($amount, Symbol::TP_RATE, 18));
-    }
-
-
-    public function getDate($time, $is_convert_num = true)
-    {
-        $year = date('Y', $time);
-        $month = date('m', $time);
-        $day = date('d', $time);
-        $yearString = '';
-        if ($is_convert_num) {
-
-            for ($i = 0; $i < strlen($year); $i++) {
-                $yearString .= $this->getNumber($year[$i]);
-            }
-
-            $month = $this->getNumber($month);
-            $day = $this->getNumber($day);
-        } else {
-            $yearString = $year;
-        }
-
-        return $yearString . '年' . $month . '月' . $day . '日';
-    }
 
     protected function getNumber($number)
     {
